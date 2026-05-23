e96806bb987a28d88d42cbe2a302faaf8e63c40d
Ayyaraabl <sarhnblmr@gmail.com>
2026-05-23 10:36:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,61 @@ st.set_page_config(
     page_icon="🧪",
     layout="wide"
 )
+# CUSTOM CSS
+# =========================
+st.markdown("""
+<style>
+
+body {
+    background-color: #0f172a;
+}
+
+.main {
+    background: linear-gradient(to right, #0f172a, #1e3a8a);
+    color: white;
+}
+
+h1, h2, h3 {
+    color: #ffffff;
+    text-shadow: 0px 0px 10px cyan;
+}
+
+.card {
+    background: rgba(255,255,255,0.1);
+    padding: 20px;
+    border-radius: 20px;
+    backdrop-filter: blur(10px);
+    box-shadow: 0 0 15px rgba(0,255,255,0.5);
+    transition: 0.3s;
+    margin-bottom: 20px;
+}
+
+.card:hover {
+    transform: scale(1.03);
+    box-shadow: 0 0 25px cyan;
+}
+
+.footer {
+    text-align: center;
+    padding: 20px;
+    color: white;
+}
+
+</style>
+""", unsafe_allow_html=True)
+
+# =========================
+# SIDEBAR
+# =========================
+st.sidebar.title("🧪 Navigasi")
+
+menu = st.sidebar.radio(
+    "Pilih Menu",
+    [
+        "Home",
+        "Daftar Alat",
+        "Fungsi Alat",
+        "Kuis Kimia",
+        "Tentang Pembuat"
+    ]
+)
