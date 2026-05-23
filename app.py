5032a8c85294cc4f36369b147f77a3828a2bed5b
Ayyaraabl <sarhnblmr@gmail.com>
2026-05-23 10:34:29 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+
+# =========================
+# KONFIGURASI HALAMAN
+# =========================
+st.set_page_config(
+    page_title="Alat Laboratorium Kimia",
+    page_icon="🧪",
+    layout="wide"
+)
